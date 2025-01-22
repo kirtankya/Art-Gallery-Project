5ec167f62c1ea97fce82805efbdcbfc5e4e2ee2d
Anurag Pandey <anuragpandey8469@gmail.com>
2025-01-22 23:25:21 +0530

    fix: DB model config issue

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,8 @@ const mongoose = require("mongoose");
 const connectDatabase = () => {
   mongoose
     .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
